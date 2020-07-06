42be074a2b31064111b2196ecff3a90aef35446b
KandabadaSenarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-07-06 22:19:53 +0530

    Making Main Page Done

---
+++ main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WeChat Login and Signup</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="well">
+				<center><h1>We Chat</h1></center>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-6" style="left: 0.5%;">
+			<img src="images/chat.jpg" class="img-rounded" title="WeChat" width="650px" height="430px">
+			<div id="centered1" class="centered"><h3 style="color: white;"><span class="glyphicon glyphicon-search"></span>&nbsp&nbsp<strong>Follow your Interests.</strong></h3></div>
+		</div>
+	</div>
+</body>
+</html>
